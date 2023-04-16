47a6f78521321cfaaaa3032d56fc5bf90f566ebe
x22131825 <124701405+x22131825@users.noreply.github.com>
2023-04-16 01:18:39 +0100

    Add files via upload

---
+++ scripts/instance_os_dependencies.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+sudo apt update
+sudo apt install ruby-full
+sudo apt install wget
+wget https://aws-codedeploy-eu-west-1.s3.eu-west-1.amazonaws.com/latest/install
+chmod +x ./install
+sudo ./install auto > /tmp/logfile
+sudo service codedeploy-agent status
+
+sudo apt install -y python3-pip
+sudo apt install -y nginx
+sudo pip install --upgrade pip -r requirements.txt
+sudo apt install -y virtualenv
