54113f0d599a7a7aeed81be451845808fcb00cc4
x22131825 <124701405+x22131825@users.noreply.github.com>
2023-04-16 02:29:45 +0100

    Update instance_os_dependencies.sh

--- scripts/instance_os_dependencies.sh
+++ scripts/instance_os_dependencies.sh
@@ -1,11 +1,4 @@
 #!/usr/bin/env bash
-sudo apt update
-sudo apt install ruby-full
-sudo apt install wget
-wget https://aws-codedeploy-eu-west-1.s3.eu-west-1.amazonaws.com/latest/install
-chmod +x ./install
-sudo ./install auto > /tmp/logfile
-sudo service codedeploy-agent status
 
 sudo apt install -y python3-pip
 sudo apt install -y nginx
